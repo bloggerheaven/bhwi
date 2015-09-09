9d3e147bcc0298768a21292195b86fab7969afd0
Cyril Kyburz <cyril.kyburz@renuo.ch>
2015-09-09 07:29:28 +0200

    new structure

--- src/bhwi.ts
+++ src/bhwi.ts
@@ -1,37 +1,6 @@
 /// <reference path="../typings/tsd.d.ts" />
 
-class BhwiUser {
-  id: number;
-  client_id: number;
-  url: string;
-
-  constructor(id: number, client_id: number) {
-    this.client_id = client_id;
-    this.id = id;
-    this.url = this._buildUrl()
-  }
-
-  _buildUrl() {
-    return 'https://api.instagram.com/v1/users/' + this.id + '/media/recent/?client_id=' + this.client_id + '&callback=?';
-  }
-}
-
-class Bhwi {
-  user: BhwiUser;
-
-  constructor(id: number, client_id: number) {
-    this.user = new BhwiUser(id, client_id);
-    this._createWidget();
-  }
-
-  _createWidget() {
-    jQuery.getJSON(this.user.url).done((insta_posts) => {
-      jQuery.each(insta_posts.data, (index, insta_posts) => {
-        this._buildWidget(insta_posts.link, insta_posts.images.standard_resolution.url).appendTo('#bhwi');
-      });
-    });
-  }
-
+class BhwiHelper {
   _buildLink(link_url: string) {
     return jQuery('<a>').attr({href: link_url, target: '_blank'});
   }
@@ -42,5 +11,71 @@ class Bhwi {
 
   _buildWidget(link_url: string, image_url: string) {
     return this._buildLink(link_url).append(this._buildImage(image_url));
+  }
+}
+
+class BwhiSlider {}
+
+class BhwiImage {
+  link: string;
+  standard: string;
+  thumbnail: string;
+
+  constructor(link: string, standard: string, thumbnail: string) {
+    this.link = link;
+    this.standard = standard;
+    this.thumbnail = thumbnail;
+  }
+}
+
+class BhwiImages {
+  images: BhwiImage[];
+
+  constructor() {
+    this.images = new Array;
+  }
+
+  addImage(image: BhwiImage) {
+    this.images.push(image);
+  }
+
+  addBuildImage(link: string, standard: string, thumbnail: string) {
+    this.addImage(new BhwiImage(link, standard, thumbnail));
+  }
+}
+
+class BhwiUser {
+  id: number;
+  client_id: number;
+  url: string;
+
+  constructor(id: number, client_id: number) {
+    this.client_id = client_id;
+    this.id = id;
+    this.url = this._buildUrl();
+  }
+
+  _buildUrl() {
+    return 'https://api.instagram.com/v1/users/' + this.id + '/media/recent/?client_id=' + this.client_id + '&callback=?';
+  }
+}
+
+class Bhwi {
+  bhwi_user: BhwiUser;
+  bhwi_images: BhwiImages;
+
+  constructor(id: number, client_id: number) {
+    this.bhwi_user = new BhwiUser(id, client_id);
+    this.bhwi_images = new BhwiImages;
+    this._fillBhwiImages();
+    console.log(this.bhwi_images);
+  }
+
+  _fillBhwiImages() {
+    jQuery.getJSON(this.bhwi_user.url).done((insta_posts) => {
+      jQuery.each(insta_posts.data, (index, insta_posts) => {
+        this.bhwi_images.addBuildImage(insta_posts.link, insta_posts.images.standard_resolution.url, insta_posts.images.thumbnail.url);
+      });
+    });
   }
 }
